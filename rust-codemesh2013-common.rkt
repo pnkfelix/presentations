f974090aa199b7072d3b364f2176a7a03f685484
Felix S. Klock II <pnkfelix@pnkfx.org>
2013-12-05 04:01:14 +0100

    A rust-oriented syntax highlight hack.

--- rust-codemesh2013-common.rkt
+++ rust-codemesh2013-common.rkt
@@ -1,8 +1,42 @@
 #lang racket
 (require slideshow)
-(provide outline tt/nl)
+(provide outline rust-tt/nl tt/nl rust-tt recolorize)
 
 (current-main-font "Open Sans")
+
+(define (recolorize pict-thunk color)
+  (colorize (parameterize ((black-and-white #t)) (pict-thunk)) color))
+
+(define rust-keywords
+  '("_" "fn" "let" "self" "as" "break" "const" "do" "else" "enum" "extern"
+    "false" "fn" "for" "if" "impl" "let" "loop" "match" "mod" "mut" "once"
+    "priv" "pub" "ref" "return" "static" "self" "struct" "super"
+    "true" "trait" "type" "unsafe" "use" "while" "in" "continue" "proc"))
+
+(define (concat op l)
+  (cond ((null? (cdr l)) (car l))
+        (else (op (car l) (concat op (cdr l))))))
+
+(define (regexp-split-but-keep re string)
+  (let ((pos* (regexp-match-positions* re string)))
+    (let loop ((accum '()) (p pos*) (last 0))
+      (cond ((null? p)
+             (reverse (cons (substring string last (string-length string)) accum)))
+            (else
+             (let* ((m (car p))
+                    (s (car m))
+                    (e (cdr m)))
+               (let ((prev (cons (substring string last s) accum)))
+                 (loop (cons (substring string s e) prev) (cdr p) e))))))))
+
+(define (rust-tt string)
+  (define (one tok)
+    (cond ((member tok rust-keywords)                      (colorize (tt tok) "dark green"))
+          ((regexp-match #rx"^[a-zA-Z][a-zA-Z0-9]*!$" tok) (colorize (tt tok) "maroon"))
+          ((regexp-match #rx"^[0-9][0-9_]*$" tok)          (colorize (tt tok) "blue"))
+          (else                                            (tt tok))))
+  (concat hbl-append
+          (map one (regexp-split-but-keep #rx"[,.&<=>{};: ]|\\(|\\)" string))))
 
 (define outline
   (let ((sub-para (lambda l (para #:width (* 3/4 (current-para-width)) l))))
@@ -38,3 +72,6 @@
 
 (define (tt/nl string)
   (apply vl-append (map tt (fragment-at-newlines string))))
+
+(define (rust-tt/nl string)
+  (apply vl-append (map rust-tt (fragment-at-newlines string))))
