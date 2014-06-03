e761def344a7647062454cc7b6f473205c9fd602
Felix S. Klock II <pnkfelix@pnkfx.org>
2014-06-03 15:12:43 +0200

    add copyright info to files in repo.

--- rust-codemesh2013-sect1.rkt
+++ rust-codemesh2013-sect1.rkt
@@ -1,4 +1,14 @@
 #lang racket
+
+;; Copyright 2014 Felix S. Klock II. See the COPYRIGHT
+;; file at the top-level directory of this distribution.
+;;
+;; Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+;; http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+;; <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+;; option. This file may not be copied, modified, or distributed
+;; except according to those terms.
+
 ;(current-command-line-arguments '#("-s" "800" "600"))
 (require slideshow)
 (require "rust-codemesh2013-common.rkt")
--- slideshow-play.rkt
+++ slideshow-play.rkt
@@ -1,5 +1,14 @@
 #lang slideshow
 
+;; Copyright 2014 Felix S. Klock II. See the COPYRIGHT
+;; file at the top-level directory of this distribution.
+;;
+;; Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+;; http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+;; <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+;; option. This file may not be copied, modified, or distributed
+;; except according to those terms.
+
 (define blue-fish (standard-fish (* 3 gap-size) (* 2 gap-size) 
                                  #:direction 'right 
                                  #:color "blue" 
