5a2241f15beef74a0942c9726d309b9cb6fd0958
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-07-01 00:45:31 +0200

    Checkpoint state, no real content here yet.

--- curry-on2015/build.rs
+++ curry-on2015/build.rs
@@ -2,9 +2,17 @@ extern crate tango;
 extern crate cmd_pandoc as pandoc;
 
 fn main() {
+    use pandoc::PandocOption as O;
+    use pandoc::OutputFormat as F;
+
     tango::process_root().unwrap();
 
     // We must run pandoc's processing after tango since tango may
     // generate `.md` files that we want to process.
-    pandoc::run_pandoc(".", "index").unwrap();
+    pandoc::run_pandoc(".", "../curry-on2015",
+                       &[O::Standalone,
+                         O::To(F::revealjs),
+                         // O::Var("theme=clean", None),
+
+                         ]).unwrap();
 }
--- curry-on2015/src/lib.md
+++ curry-on2015/src/lib.md
@@ -1,5 +1,14 @@
+% Rust: A Type System You Didn't Know You Wanted
+% Felix Klock (`@pnkfelix`)
+% Mozilla Research
+
+# Rust: A Type System You Didn't Know You Wanted
+
+## Hello World
+
 ```rust
 #[test]
 fn it_works() {
+    println!("Hello World");
 }
 ```
