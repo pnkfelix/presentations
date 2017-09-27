2694815d9fd696bf54a3f266fae669d83663d50c
Felix S. Klock II <pnkfelix@pnkfx.org>
2017-09-27 15:42:11 +0200

    prepared for separating the end-of-story from end-of-presentation.

---
+++ rustfest-zurich-2017/src/l00_end_of_it.md
@@ -0,0 +1,14 @@
+# The End of It
+
+## Child, what day is it?
+
+## Epochery
+
+```rust
+fn main() {
+    do catch {
+        panic!("Scary!");
+    }
+    println!("Spirits!");
+}
+```
--- rustfest-zurich-2017/src/lib.rs
+++ rustfest-zurich-2017/src/lib.rs
@@ -1,8 +1,11 @@
+#![feature(catch_expr)]
+
 pub mod a00;
 pub mod c00_nikos_ghost;
 pub mod e00_epochs_past;
 pub mod g00_epochs_present;
 pub mod i00_epochs_future;
+pub mod l00_end_of_it;
 pub mod z00;
 
 #[cfg(test)]
